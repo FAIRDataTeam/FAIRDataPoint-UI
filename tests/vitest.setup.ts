48628a8146c2cba5c2cabed350efa09d14ffd052
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-07-13 16:43:18 +0200

    separate testRuntimeConfig for cleaner type checking

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -4,6 +4,8 @@ import { RuntimeConfig } from '../src/config'
 
 const TEST_FDP_BASE_URL = 'http://localhost'
 
+const testRuntimeConfig: RuntimeConfig = { fdpBaseUrl: TEST_FDP_BASE_URL }
+
 /**
  * Mock the config module to return a value without loading from a JSON file.
  * This is a global setup, so we do not need to call `loadRuntimeConfig` in individual test files.
@@ -13,7 +15,7 @@ vi.mock(
   () => {
     return {
       loadRuntimeConfig: vi.fn().mockResolvedValue(undefined),
-      getRuntimeConfig: vi.fn().mockReturnValue({ fdpBaseUrl: TEST_FDP_BASE_URL } as RuntimeConfig),
+      getRuntimeConfig: vi.fn().mockReturnValue(testRuntimeConfig),
     }
   }
 )
