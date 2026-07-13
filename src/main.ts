ea1cf439f80207522854d3976c971b2322541d8c
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-07-13 16:06:58 +0200

    import config without .ts extension, for consistency

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import router from './router'
 import { authReady } from './composables/useAuth'
 
 import './assets/main.css'
-import { loadRuntimeConfig } from '@/config.ts'
+import { loadRuntimeConfig } from '@/config'
 
 const app = createApp(App)
 
-// Load runtime configuration from JSON file. Afterwards, we can use getRuntimeConfig() to access the result.
+// Load runtime configuration from a JSON file.
+// Afterwards, we can use getRuntimeConfig() to access the result at any time.
 await loadRuntimeConfig()
 
 authReady.then(() => {
