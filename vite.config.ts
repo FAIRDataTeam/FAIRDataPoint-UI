a43b99ff391af1498d3a322fa97870d01ecd9712
Dennis <29799340+dennisvang@users.noreply.github.com>
2026-08-20 15:25:40 +0200

    Single source of truth for version (#52)
    * remove name and version from package.json (these are bound to get out-of-sync at some point, and they are not required because we don't publish an npm package: https://docs.npmjs.com/cli/v10/configuring-npm/package-json?v=true#version)
    
    * replace version from package.json by gitDescribeVersion() in vite.config.ts
    
    * fail fast so we don't need to add a test for the gitDescribeVersion
    
    * do not ignore .git from docker build context
      
      The .git dir is only needed to get a version description based on git tags in the build stage.
      The .git dir is not copied to the final image.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 /// <reference types="vitest/config" />
 
+import { strict as assert } from 'node:assert'
+import { spawnSync } from 'node:child_process'
 import * as fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
@@ -8,8 +10,6 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig, HtmlTagDescriptor, ViteDevServer } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
-
-import { version } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,7 +21,7 @@ export default defineConfig({
     },
   },
   define: {
-    __APP_VERSION__: JSON.stringify(version),
+    __APP_VERSION__: JSON.stringify(gitDescribeVersion()),
     __APP_BUILT_AT__: JSON.stringify(new Date().toISOString()),
   },
   // vitest options
@@ -63,6 +63,25 @@ function clientConfigOverridePlugin() {
       })
     },
   }
+}
+
+/**
+ * Returns a version string obtained from `git describe`, in long format, for example, `'v1.2.3-0-gfc7760f'`.
+ * Fails intentionally if the `git describe` call fails or if its output is falsy.
+ */
+function gitDescribeVersion(): string {
+  const { stdout, stderr, status } = spawnSync(
+    'git',
+    [
+      'describe',
+      '--tags', // include the lightweight tags created by e.g. github
+      '--always', // use abbreviated commit hash if there are no tags at all
+      '--long', // use long format even if the current commit is tagged ('v1.2.3-0-gfc7760f' instead of 'v1.2.3')
+    ],
+    { encoding: 'utf8' },
+  )
+  assert(stdout, `git describe call failed (exit code: ${status}, stderr: ${stderr?.trim()})`)
+  return stdout.trim()
 }
 
 /**
